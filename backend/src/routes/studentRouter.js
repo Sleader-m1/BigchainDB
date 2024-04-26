c849ab83ae3535b54bf26c1757873ad76304439b
bittick <prokhorovm.v@yandex.ru>
2024-04-26 16:15:22 +0300

    aboboa

--- backend/src/routes/studentRouter.js
+++ backend/src/routes/studentRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {createNewStudent,getStudentDetails,updateStudent,deleteStudent} = require("../controllers/studentController");
+const {createNewStudent,getStudentDetails,updateStudent,deleteStudent, genKeyPairController} = require("../controllers/studentController");
 
 router.route("/").post(createNewStudent);
+router.route("/gen_key_pair").get(genKeyPairController);
 router.route("/:nic").get(getStudentDetails).patch(updateStudent).delete(deleteStudent);
 
 module.exports = router;
