6e1061d21fe786ba67d16caeced7f2fc32fa68a4
bittick <prokhorovm.v@yandex.ru>
2024-05-01 17:22:44 +0300

    fix1

--- backend/src/controllers/bigcheinController.js
+++ backend/src/controllers/bigcheinController.js
@@ -1,54 +1,49 @@
 const { Ed25519Keypair, Transaction, Connection } = require('bigchaindb-driver');
 const base58 = require('bs58');
-const crypto = require('crypto');
-const { Ed25519Sha256 } = require('crypto-conditions');
+const API_PATH = 'http://83.222.9.173:9984/api/v1/';
 
-const API_PATH = 'http://95.164.32.14:9984/';
+// Функция для генерации ключевой пары из приватного ключа
+function generateKeyPairFromPrivateKey(privateKey) {
+    const privateKeyBuffer = base58.decode(privateKey);
+    return new Ed25519Keypair(privateKeyBuffer);
+}
 
-class UploadController {
-    constructor() {
-        this.conn = new Connection(API_PATH);
-    }
+// Функция для загрузки файла и отправки транзакции в BigchainDB
+async function uploadFile(fileBuffer, fileName, privateKey) {
+    try {
+        const fileContentBase64 = fileBuffer.toString('base64');
+        const keyPair = generateKeyPairFromPrivateKey(privateKey);
 
-    async uploadFile(fileBuffer, fileName, privateKey) {
-        try {
-            const fileContentBase64 = fileBuffer.toString('base64');
-            const keyPair = this._generateKeyPairFromPrivateKey(privateKey);
+        const assetData = {
+            fileContent: fileContentBase64,
+            filename: fileName
+        };
 
-            const assetData = {
-                fileContent: fileContentBase64,
-                filename: fileName
-            };
+        const metaData = {
+            name: fileName
+        };
 
-            const metaData = {
-                name: fileName
-            };
+        const txCreate = Transaction.makeCreateTransaction(
+            assetData,
+            metaData,
+            [Transaction.makeOutput(Transaction.makeEd25519Condition(keyPair.publicKey))],
+            keyPair.publicKey
+        );
 
-            const txCreate = Transaction.makeCreateTransaction(
-                assetData,
-                metaData,
-                [Transaction.makeOutput(Transaction.makeEd25519Condition(keyPair.publicKey))],
-                keyPair.publicKey
-            );
+        const txSigned = Transaction.signTransaction(txCreate, privateKey);
 
-            const txSigned = Transaction.signTransaction(txCreate, privateKey);
-
-            const retrievedTx = await this.conn.postTransactionCommit(txSigned);
-            return {
-                success: true,
-                transactionId: retrievedTx.id,
-                message: 'Transaction successfully posted.'
-            };
-        } catch (error) {
-            console.error('Error in uploadFile:', error);
-            throw new Error('Failed to upload file and post transaction.');
-        }
-    }
-
-    _generateKeyPairFromPrivateKey(privateKey) {
-        const privateKeyBuffer = base58.decode(privateKey);
-        return new Ed25519Keypair(privateKeyBuffer);
+        const conn = new Connection(API_PATH);
+        const retrievedTx = await conn.postTransactionCommit(txSigned);
+        return {
+            success: true,
+            transactionId: retrievedTx.id,
+            message: 'Transaction successfully posted.'
+        };
+    } catch (error) {
+        console.error('Error in uploadFile:', error);
+        throw new Error('Failed to upload file and post transaction.');
     }
 }
 
-module.exports = {UploadController};
+
+module.exports = {uploadFile}
--- backend/src/routes/studentRouter.js
+++ backend/src/routes/studentRouter.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const {createNewStudent,getStudentDetails,updateStudent,deleteStudent, genKeyPairController} = require("../controllers/studentController");
 
-const {UploadController} = require("../controllers/bigcheinController")
+const {uploadFile} = require("../controllers/bigcheinController")
 
-router.route('/upload_file').post(UploadController);
+router.route('/upload_file/').post(uploadFile);
 router.route("/").post(createNewStudent);
 router.route("/gen_key_pair").get(genKeyPairController);
 router.route("/:nic").get(getStudentDetails).patch(updateStudent).delete(deleteStudent);
